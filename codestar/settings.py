564a1c28456c8d4dc02639fc16e860c019f08f18
arnold buiuca <arnoldbuiuka@gmail.com>
2024-02-04 17:57:51 +0000

    create the post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("W8YULvtFJi")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-arnoldkoss-djangoblog-vb0oto8ube9.ws-eu108.gitpod.io','.herokuapp.com']
 
@@ -89,6 +89,12 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.herokuapp.com",
+    "https://*.gitpod.io"
+]
 
 
 # Password validation
