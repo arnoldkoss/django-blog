bc6636ee951549aa3a91599bc3407949c9dccbf5
arnold buiuca <arnoldbuiuka@gmail.com>
2024-02-06 18:55:07 +0000

    start coding for the views

--- blog/models.py
+++ blog/models.py
@@ -33,6 +33,8 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    
+
 
     class Meta:
         ordering = ["created_on"]
