4154e57bee848bd113024ee4a68c6a99595255ca
arnold buiuca <arnoldbuiuka@gmail.com>
2024-02-16 12:38:07 +0000

    wire upp the form to the back end

--- about/admin.py
+++ about/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
-from .models import About
+from .models import About, CollaborateRequest
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(About)
 class AboutAdmin(SummernoteModelAdmin):
-    
     summernote_fields = ('content',)
 
 
 
-# Register your models here.
+@admin.register(CollaborateRequest)
+class CollaborateRequestAdmin(admin.ModelAdmin):
 
+    list_display = ('message', 'read',)
